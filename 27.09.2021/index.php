dae3d932e09adcd905072ecf6aa403fcc163375d
Krainov Vadim <kraynov_91@list.ru>
2021-10-15 15:05:11 +0800

    11.10.2021

--- 27.09.2021/index.php
+++ 27.09.2021/index.php
@@ -32,17 +32,40 @@ include ("ini/header.php");
             <img class="smile" src="image/photo.jpg" alt="">
         </div>
         <div class="flex-information">
-            <div class="fio">Крайнов Вадим
+            <div class="fio">
+                <?
+                $fio = "Крайнов Вадим";
+                echo $fio;
+                ?>
             </div>
             <div class="text">
                 <div class="text-1">
-                    Мои навыки: html,css <br>
-                    До этого занимался веб-дизайном на фрилансе (лет 6-7), также в одной веб студии 2 года. <br>
-                    Занятие первое пропустил к сожалению из-за техническких проблем. Сейчас посмотрел в записи. <br>
-                    Объясняете хорошо, на данный момент более менее все понятно. Ритм обучения устраивает. Спасибо за внимание))<br>
+                    <? $text1 = "Мои навыки: html,css. <br>
+                    До этого занимался веб-дизайном на фрилансе (лет 6-7) также в одной веб студии 2 года.<br>
+                    Занятие первое пропустил к сожалению из-за техническких проблем. Сейчас посмотрел в записи.<br>
+                    Объясняете хорошо на данный момент более менее все понятно. Ритм обучения устраивает.<br>
+                    Спасибо за внимание))";
+                    $text1Arr = explode (', ', $text1);
+                    $text1Arr[0] = "<span style= 'color: #551a8b'>$text1Arr[0]</span>";
+                    $textcolor = implode('. ', $text1Arr);
+                    echo $textcolor;
+                    ?>
                 </div>
                 <div class="text-2">
-                    Все супееееер)
+                    <?
+                    $text2 =  "Все супееееер, отлично)";
+                    $text2Arr = explode (' ', $text2);
+                    foreach ($text2Arr as $a => $b) {
+                        if ($a % 2 == 0) {
+                            $text2Arr[$a] = "<span style= 'color: #551a8b'>$b</span>";
+                        }
+                        else{
+                            $text2Arr[$a] = "<span style= 'color: coral'>$b</span>";
+                        }
+                    }
+                    $text2color = implode('', $text2Arr);
+                    echo $text2color;
+                    ?>
                 </div>
             </div>
         </div>
@@ -89,7 +112,22 @@ include ("ini/header.php");
 </main>
 
 <footer class="footer">
-
+    <div class="flex-container-2">
+        <?
+        /*echo "На данный момент время: $timeDisplayed";*/
+        $alltext = $fio.$text1.$text2;
+        $publicCount = preg_match_all('/[аеёиоуэюя]/iu', $alltext);
+        echo "Количество гласных на данной странице $publicCount".'<br>';
+        $textCount = str_word_count($alltext);
+        echo "Количество слов на данной странице $textCount".'<br>';
+        $strBirthday = '1991-11-11';
+        $strTime = date("Y-m-d");
+        $Birthday = date_create($strBirthday);
+        $time = date_create($strTime);
+        $interval = date_diff($Birthday, $time);
+        echo "Разница количества дней равна" .$interval ->format('%a');
+        ?>
+    </div>
 </footer>
 </body>
 </html>
