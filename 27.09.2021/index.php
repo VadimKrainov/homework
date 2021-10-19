9b09ee1427f5fcd3d1fd816a10afd492e14b3a5d
Krainov Vadim <kraynov_91@list.ru>
2021-10-19 23:47:28 +0800

    functions

--- 27.09.2021/index.php
+++ 27.09.2021/index.php
@@ -1,19 +1,20 @@
 <?
+function time_style() {
+    $night = date("H:i:s");
+    if ($night >= "08:00" and $night <= "23:59") {
+        echo "<link rel='stylesheet' href='css/style.css'>";
+    } else {
+        echo "<link rel='stylesheet' href='css/style_night.css'>";
+    }
+}
+time_style();
 $night = date("H:i:s");
 echo "$night";
-if($night >= "08:00" and $night <= "23:59")
-{
-    echo "<link rel='stylesheet' href='css/style.css'>";
-}
-else{
-    echo "<link rel='stylesheet' href='css/style_night.css'>";
-}
 ?>
 
 <!doctype html>
 <html lang="ru">
 <head>
-    <link rel="stylesheet" href="<?echo $night;?>">
     <meta charset="UTF-8">
     <meta content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
           name="viewport">
@@ -35,25 +36,34 @@ include ("ini/header.php");
             <div class="fio">
                 <?
                 $fio = "Крайнов Вадим";
+                function fio($fio) {
                 echo $fio;
+                }
+                fio($fio);
                 ?>
             </div>
             <div class="text">
                 <div class="text-1">
-                    <? $text1 = "Мои навыки: html,css. <br>
-                    До этого занимался веб-дизайном на фрилансе (лет 6-7) также в одной веб студии 2 года.<br>
-                    Занятие первое пропустил к сожалению из-за техническких проблем. Сейчас посмотрел в записи.<br>
-                    Объясняете хорошо на данный момент более менее все понятно. Ритм обучения устраивает.<br>
-                    Спасибо за внимание))";
-                    $text1Arr = explode (', ', $text1);
-                    $text1Arr[0] = "<span style= 'color: #551a8b'>$text1Arr[0]</span>";
-                    $textcolor = implode('. ', $text1Arr);
-                    echo $textcolor;
+                    <?
+                    $text1 = 'Мои навыки: html,css' . '<br>'
+                    . 'До этого занимался веб-дизайном на фрилансе (лет 6-7) также в одной веб студии 2 года' . '<br>'
+                    . 'Занятие первое пропустил к сожалению из-за техническких проблем. Сейчас посмотрел в записи' . '<br>'
+                    . 'Объясняете хорошо на данный момент более менее все понятно. Ритм обучения устраивает' . '<br>'
+                    . 'Спасибо за внимание))';
+                    function text1($text1)
+                    {
+                        $text1Arr = explode(', ', $text1);
+                        $text1Arr[0] = "<span style= 'color: #551a8b'>$text1Arr[0]</span>";
+                        $textcolor = implode('. ', $text1Arr);
+                        echo $textcolor;
+                    }
+                    text1($text1);
                     ?>
                 </div>
                 <div class="text-2">
                     <?
                     $text2 =  "Все супееееер, отлично)";
+                    function text2($text2) {
                     $text2Arr = explode (' ', $text2);
                     foreach ($text2Arr as $a => $b) {
                         if ($a % 2 == 0) {
@@ -65,6 +75,8 @@ include ("ini/header.php");
                     }
                     $text2color = implode('', $text2Arr);
                     echo $text2color;
+                    }
+                    text2($text2);
                     ?>
                 </div>
             </div>
@@ -114,18 +126,33 @@ include ("ini/header.php");
 <footer class="footer">
     <div class="flex-container-2">
         <?
-        /*echo "На данный момент время: $timeDisplayed";*/
+        //Количество слов на данной странице
         $alltext = $fio.$text1.$text2;
-        $publicCount = preg_match_all('/[аеёиоуэюя]/iu', $alltext);
-        echo "Количество гласных на данной странице $publicCount".'<br>';
         $textCount = str_word_count($alltext);
+        function textCount($textCount) {
         echo "Количество слов на данной странице $textCount".'<br>';
+        }
+        textCount($textCount);
+
+        //Количество гласных на данной странице
+        $publicCount = preg_match_all('/[аеёиоуэюя]/iu', $alltext);
+        function publicCount($publicCount)
+        {
+            echo "Количество гласных на данной странице $publicCount" . '<br>';
+        }
+        publicCount($publicCount);
+
+        //Разница количества дней равна
         $strBirthday = '1991-11-11';
         $strTime = date("Y-m-d");
         $Birthday = date_create($strBirthday);
         $time = date_create($strTime);
         $interval = date_diff($Birthday, $time);
-        echo "Разница количества дней равна" .$interval ->format('%a');
+        function interval($interval)
+        {
+            echo "Разница количества дней равна" . $interval->format('%a');
+        }
+        interval($interval);
         ?>
     </div>
 </footer>
